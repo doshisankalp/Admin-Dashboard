7e2d025f079b06577bffee3c5d3e401e922f0b97
Omkar Nath Singh <omkar@glabbr.com>
2017-06-07 16:50:34 +0530

    First Push

--- display.php
+++ display.php
@@ -9,48 +9,77 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.css"/>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.js"></script>
+
 </head>
 
 <body>
 <p>
 
-<form method="post" >
-    Enter Number of rows to display per page: <input type=number name="NumRows">
-    <input type="submit" value="Display Database entries" ><br>
-</form>
-</p>
+
+<!--<form method="post" >-->
+<!--    Enter Number of rows to display per page: <input type=number name="NumRows">-->
+<!--    <input type="submit" name="Init" value="Display Database entries" ><br>-->
+<!--    --><?php //    main();    ?>
+<!--<br>-->
+<!--    <input type="submit" name="Next" value="Next">-->
+<!--    <input type="submit" name="Previous" value="Previous">-->
+<!---->
+<!--</form>-->
+
 
 
 <?php
-//include("/db/connectdb.php");
-$servername = "localhost";
-$username = "root";
-$password = "mysql";
-$database = "employees";
+$numrow = 0;
+$index = 0;
 
-//create conntion
-$conn = mysqli_connect($servername, $username, $password,$database);
-if(isset($_POST['NumRows'])) {
-    $_COOKIE['start']=0;
-    $_COOKIE['rows']=$_POST['NumRows'];
+if(isset($_POST['Init'])){
+    $index = 0;
+    $numrow = $_POST['NumRows'];
+}
+if(isset($_POST['Next'])){
+    $index=$_POST['CurrentIndex']+1;
+    $numrow = $_POST['CurrentRows'];
+}
 
-    $start=$_COOKIE['start'];
-    $rows=$_COOKIE['rows'];
-    $sqlquery = "select * from employees limit $start,$rows";
-    $result = mysqli_query($conn,$sqlquery);
+if(isset($_POST['Previous'])){
+    $index=$_POST['CurrentIndex']-1;
+    $numrow = $_POST['CurrentRows'];
+}
+
+
+    echo "<form method=\"post\" >
+    Enter Number of rows to display per page: <input type=number name=\"NumRows\">
+    <input type=\"submit\" name=\"Init\" value=\"Display Database entries\" ><br>
+    <?php     main();    ?>
+<br>
+    <input type=\"submit\" name=\"Next\" value=\"Next\">
+    <input type=\"submit\" name=\"Previous\" value=\"Previous\">
+    <input type=\"hidden\" name=\"CurrentIndex\" value=\"".$index."\">
+    <input type=\"hidden\" name=\"CurrentRows\" value=\"".$numrow."\">
+</form>";
+
+    include("connectdb.php");
+
+    $sqlquery = "select * from employees limit ".$index*$numrow.",".$numrow;
+    echo $sqlquery;
+    if (!empty($conn)) {
+        $result = mysqli_query($conn, $sqlquery);
+    }
     $count = mysqli_num_rows($result);
 
     if ($count > 0) {
-        while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-            echo "emp no: " . $row["emp_no"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            echo "emp no: " . $row["emp_no"] . " - Name: " . $row["first_name"] . " " . $row["last_name"] . "<br>";
         }
     } else {
         echo "0 results";
     }
 
 
-
-}
 ?>
 </body>
 </html>
