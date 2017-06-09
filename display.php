527fd0951479f686208feb7257145a0dc11ceab5
Omkar Nath Singh <omkar@glabbr.com>
2017-06-09 17:03:29 +0530

    Display Working

--- display.php
+++ display.php
@@ -1,31 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: omkar
- * Date: 9/6/17
- * Time: 2:08 PM
- */
-include("connectdb.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Display Table using Ajax</title>
+    <script language="JavaScript" src="./js/DisplayLib.js"></script>
+    <script language="JavaScript" src="./js/EditLib.js"></script>
 
-$index=$_POST['startindex'];
-$numrow=$_POST['numrow'];
+</head>
+<body>
+<form>
 
-$sqlquery = "select * from employees limit " . $index . "," . $numrow;
-echo $sqlquery;
+    Enter Number of Rows:  <input type="number" name="numrow" id="numrow"><br>
 
-if (!empty($conn)) {
-    $result = mysqli_query($conn, $sqlquery);
-}
-$count = mysqli_num_rows($result);
+    <button type="button" onclick="show(0)">Display</button><br/><br/>
 
-if ($count > 0) {
-    echo "<table>";
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        echo "<tr>";
-        echo "<td>" . $row["emp_no"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["last_name"] . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
+    <button type="button" onclick="show(-1)">Previous</button>
+    <button type="button" onclick="show(1)">Next</button>
+
+</form>
+<div id="display"></div>
+</body>
+</html>
