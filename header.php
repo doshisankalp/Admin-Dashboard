9c25594a56d1133fb4ff1ea892de7e7fe9b4d8e5
Omkar Nath Singh <omkar@glabbr.com>
2017-06-29 12:05:13 +0530

    removed amazon bubbles code.
    Signed-off-by: Omkar Nath Singh <omkar@glabbr.com>

--- header.php
+++ header.php
@@ -1,9 +1,3 @@
-<!--<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>-->
-<!--<script type="text/javascript" src="http://s3.amazonaws.com/codecademy-content/courses/hour-of-code/js/alphabet.js"></script>-->
-<!---->
-<!--<canvas id="myCanvas"></canvas>-->
-<!--<script type="text/javascript" src="./js/bubbles.js"></script>-->
-<!--<script type="text/javascript" src="./js/main.js"></script>-->
 <div>
     <h1 align="center">
         Neeti Solutions
