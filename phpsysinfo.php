f63feddb8279f27d82923b7b921dd00d97e9daa9
Omkar Nath Singh <omkar@glabbr.com>
2017-06-29 15:01:14 +0530

    Adapted Changes for External Libraries.
    Signed-off-by: Omkar Nath Singh <omkar@glabbr.com>

--- phpsysinfo.php
+++ phpsysinfo.php
@@ -4,7 +4,7 @@
 </head>
 <body style="margin:0px;padding:0px;overflow:hidden">
 <div><?php include("./navigation.php"); ?></div>
-<iframe src="../phpsysinfo/index.php" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+<iframe src="./phpsysinfo/index.php" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
 
 </body>
 </html>
